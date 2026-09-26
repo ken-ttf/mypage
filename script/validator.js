98a65af0efa8a062c40a1e72a813fbe32c637345
ken-ttf <ken_ttf@nifty.com>
2026-09-26 19:50:53 +0900

    新 index のW3C CSS 検証サービスをアイコン表示する形に修正。

--- script/validator.js
+++ script/validator.js
@@ -7,4 +7,4 @@
 //
 ////////////////////////////////////////////////////////////////////////
 document.writeln("<p><a href='http:\/\/validator.w3.org\/check?uri=" + document.location + "'>Nu Html Checker<\/a><\/p>");
-document.writeln("<p><a href='http:\/\/jigsaw.w3.org\/css-validator\/validator?uri=" + document.location + "'>W3C CSS 検証サービス<\/a><\/p>");
+document.writeln("<p><a href='http:\/\/jigsaw.w3.org\/css-validator\/validator?uri=" + document.location + "'><img src='http://jigsaw.w3.org/css-validator/images/vcss-blue' alt='Valid CSS!' width='88' height='31'><\/a><\/p>");
