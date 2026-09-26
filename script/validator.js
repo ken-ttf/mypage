aaeca91aa8c7655970b1720dd0b4b45e2aa8e572
ken-ttf <ken_ttf@nifty.com>
2026-09-26 19:29:23 +0900

    新 index 関連で数点の修正を実施。

--- script/validator.js
+++ script/validator.js
@@ -6,5 +6,5 @@
 //		<script type="text/javascript" src="./script/validator.js"></script>
 //
 ////////////////////////////////////////////////////////////////////////
-document.writeln("<a href='http:\/\/validator.w3.org\/check?uri=" + document.location + "'><img src='http:\/\/www.w3.org\/Icons\/valid-xhtml11' alt='Valid XHTML 1.1!' width='88' height='31' \/><\/a>");
-document.writeln("<a href='http:\/\/jigsaw.w3.org\/css-validator\/validator?uri=" + document.location + "'><img src='http:\/\/jigsaw.w3.org\/css-validator\/images\/vcss' alt='Valid CSS!' width='88' height='31' \/><\/a>");
+document.writeln("<p><a href='http:\/\/validator.w3.org\/check?uri=" + document.location + "'>Nu Html Checker<\/a><\/p>");
+document.writeln("<p><a href='http:\/\/jigsaw.w3.org\/css-validator\/validator?uri=" + document.location + "'>W3C CSS 検証サービス<\/a><\/p>");
